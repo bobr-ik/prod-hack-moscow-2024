146b5fab2d834346a14f26f9eb0f2dc6af0c544a
bobr-ik <aavramenko804@gmail.com>
2024-11-09 10:05:11 +0300

    добавил orm для таблиц для путешествий

--- backend/data/models.py
+++ backend/data/models.py
@@ -2,6 +2,7 @@ from typing import Optional
 from sqlalchemy import Table, Column, Integer, String, MetaData, Numeric, ForeignKey, text, BigInteger
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from data.database import Base
+from datetime import datetime, timezone
 
 
 metadata_obj = MetaData()
@@ -14,4 +15,25 @@ class DebtsHistoryORM(Base):
     f_tg_tag_lender: Mapped[str] = mapped_column(String(50))
     f_tg_tag_debtor: Mapped[str] = mapped_column(String(50))
     f_event_name: Mapped[str] = mapped_column(String(50))
-    f_event_date: Mapped[str] = mapped_column(String(50))
+    f_event_date: Mapped[datetime] = mapped_column(datetime.now(timezone.utc))
+
+
+class TripsORM(Base):
+    __tablename__ = "t_trips"
+    f_id: Mapped[int] = mapped_column(primary_key=True)
+    f_trip_name: Mapped[str] = mapped_column(String(50))
+    f_start_date: Mapped[datetime] = mapped_column(datetime.now(timezone.utc))
+    f_end_date: Mapped[Optional[datetime]]
+    f_is_ended: Mapped[bool]
+
+
+class TripDebtsORM(Base):
+    __tablename__ = "t_trip_debts"
+    f_id: Mapped[int] = mapped_column(primary_key=True)
+    f_trip_id: Mapped[int] = mapped_column(ForeignKey("t_trips.f_id"))
+    f_debt_amount: Mapped[int]
+    f_tg_tag_lender: Mapped[str] = mapped_column(String(50))
+    f_tg_tag_debtor: Mapped[str] = mapped_column(String(50))
+    f_event_name: Mapped[str] = mapped_column(String(50))
+    f_event_date: Mapped[datetime] = mapped_column(datetime.now(timezone.utc))
+
