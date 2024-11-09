61271e361a04142b83891dd167d81e5b6b89169e
dak_dolka <andrejdolinov31@gmail.com>
2024-11-09 11:42:15 +0300

    исправил время в новых таблицах и добавил в орм

--- backend/data/models.py
+++ backend/data/models.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from sqlalchemy import Table, Column, Integer, String, MetaData, Numeric, ForeignKey, text, BigInteger
+from sqlalchemy import Table, Column, Integer, String, MetaData, Numeric, ForeignKey, text, BigInteger, DateTime
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from data.database import Base
 from datetime import datetime, timezone
@@ -15,14 +15,14 @@ class DebtsHistoryORM(Base):
     f_tg_tag_lender: Mapped[str] = mapped_column(String(50))
     f_tg_tag_debtor: Mapped[str] = mapped_column(String(50))
     f_event_name: Mapped[str] = mapped_column(String(50))
-    f_event_date: Mapped[datetime] = mapped_column(datetime.now(timezone.utc))
+    f_event_date: Mapped[DateTime] = mapped_column(DateTime(datetime.now(timezone.utc)))
 
 
 class TripsORM(Base):
     __tablename__ = "t_trips"
     f_id: Mapped[int] = mapped_column(primary_key=True)
     f_trip_name: Mapped[str] = mapped_column(String(50))
-    f_start_date: Mapped[datetime] = mapped_column(datetime.now(timezone.utc))
+    f_start_date: Mapped[DateTime] = mapped_column(DateTime[datetime.now(timezone.utc)])
     f_end_date: Mapped[Optional[datetime]]
     f_is_ended: Mapped[bool]
 
@@ -35,5 +35,5 @@ class TripDebtsORM(Base):
     f_tg_tag_lender: Mapped[str] = mapped_column(String(50))
     f_tg_tag_debtor: Mapped[str] = mapped_column(String(50))
     f_event_name: Mapped[str] = mapped_column(String(50))
-    f_event_date: Mapped[datetime] = mapped_column(datetime.now(timezone.utc))
+    f_event_date: Mapped[DateTime] = mapped_column(DateTime(datetime.now(timezone.utc)))
 
