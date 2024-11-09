e7ce9098b135a27e2e478b3b170bc9c4cb008661
bobr-ik <aavramenko804@gmail.com>
2024-11-10 00:41:29 +0300

    добавил веб-апп (тест)

--- backend/bot/keyboards.py
+++ backend/bot/keyboards.py
@@ -4,7 +4,7 @@ from aiogram.filters.callback_data import CallbackData
 start_kb = InlineKeyboardMarkup(
     inline_keyboard = [
         [InlineKeyboardButton(text='Новый счёт', callback_data='new'), InlineKeyboardButton(text='Новая группа', callback_data='new_group')],
-        [InlineKeyboardButton(text='Мои финансы', callback_data='my_bills')]
+        [InlineKeyboardButton(text='Мои финансы', web_app=WebAppInfo(url='https://158.160.85.97/'))]
         
     ], resize_keyboard=True
 )
